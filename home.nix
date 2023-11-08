3b4e38566f3c91c437c8aaf56189212f8f0a467f
Stefan Richter <sealor@users.noreply.github.com>
2023-11-08 17:29:16 +0100

    Add basic LSP and treesitter configuration

--- home.nix
+++ home.nix
@@ -28,8 +28,15 @@
     vimdiffAlias = true;
 
     extraConfig = builtins.readFile ./vimrc;
+    extraLuaConfig = builtins.readFile ./vim.lua;
     plugins = with pkgs.vimPlugins; [
       vim-lastplace vim-nix vim-gitgutter
+
+      lsp-zero-nvim mason-nvim nvim-cmp nvim-lspconfig mason-lspconfig-nvim cmp-nvim-lsp luasnip
+
+      (nvim-treesitter.withPlugins(p: with p; [
+        awk bash c diff dockerfile dot go gomod html java javascript json make markdown nix org python rust sql typescript terraform toml vue yaml
+      ])) # faster than nvim-treesitter.withAllGrammars
     ];
   };
 }
